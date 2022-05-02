827aac816811c61cc7184a74b6ba89b8f9079fe9
arshdeep singh <arshdeep4909@gmail.com>
2022-05-01 22:37:23 -0400

    implementing adding & removing to cart.
    at this point we are just updating the cart state when we click on an add or remove button. we are yet to render the cart on our app anywhere.

--- src/components/SingleProduct.js
+++ src/components/SingleProduct.js
@@ -1,8 +1,14 @@
 import React from "react";
 import { Card, Button } from "react-bootstrap";
 import Rating from "./Rating";
+import { CartState } from "../context/Context";
 
 const SingleProduct = ({ prod }) => {
+  const {
+    state: { cart },
+    dispatch,
+  } = CartState();
+
   return (
     <div className="products">
       <Card>
@@ -18,10 +24,37 @@ const SingleProduct = ({ prod }) => {
             )}
             <Rating rating={prod.ratings} />
           </Card.Subtitle>
-          <Button variant="danger">Remove from cart</Button>
-          <Button disabled={!prod.inStock}>
-            {!prod.inStock ? "out of Stock" : "Add to Cart"}
-          </Button>
+          {
+            // so if we have this product in our cart then we only want
+            // to show add to cart and not remove from cart
+            //.some checks if any elemnt of an array passes the given condition
+            //it will loop through array and check if it's id is equal to prod.id
+            cart.some((p) => p.id === prod.id) ? (
+              <Button
+                variant="danger"
+                onClick={() => {
+                  dispatch({
+                    type: "REMOVE_FROM_CART",
+                    payload: prod,
+                  });
+                }}
+              >
+                Remove from cart
+              </Button>
+            ) : (
+              <Button
+                disabled={!prod.inStock}
+                onClick={() => {
+                  dispatch({
+                    type: "ADD_TO_CART",
+                    payload: prod,
+                  });
+                }}
+              >
+                {!prod.inStock ? "out of Stock" : "Add to Cart"}
+              </Button>
+            )
+          }
         </Card.Body>
       </Card>
     </div>
