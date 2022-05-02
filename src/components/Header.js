9e08545f17d33a336e6643762391c4f2d70e32ee
arshdeep singh <arshdeep4909@gmail.com>
2022-05-01 23:19:13 -0400

    implementing the cart icon component.
    at this point when we add items to the cart we can see the items under our icon. we can also remove the item from delete icon under the list.

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,37 +6,81 @@ import {
   FormControl,
   Navbar,
   Nav,
+  Button,
 } from "react-bootstrap";
 import { FaShoppingCart } from "react-icons/fa";
 import { Link } from "react-router-dom";
+import { CartState } from "../context/Context";
+import { cartReducer } from "../context/Reducers";
+import { AiFillDelete } from "react-icons/ai";
 
 const Header = () => {
+  const {
+    state: { cart },
+    dispatch,
+  } = CartState();
+
   return (
     <Navbar bg="dark" variant="dark" style={{ height: 80 }}>
       {" "}
       <Container>
         <Navbar.Brand>
-          <Link href="/">Shopping Cart</Link>
-        </Navbar.Brand>
+          <Link href="/"> Shopping Cart </Link>{" "}
+        </Navbar.Brand>{" "}
         <Navbar.Text className="search">
           <FormControl
             style={{ width: 500 }}
             placeholder="search a product"
             className="m-auto"
           />
-        </Navbar.Text>
-
+        </Navbar.Text>{" "}
         <Nav>
           <Dropdown>
             <Dropdown.Toggle variant="success">
               <FaShoppingCart color="white" fontSize="25px" />
-            </Dropdown.Toggle>
+              <Badge style={{ padding: "0px" }}> {cart.length} </Badge>{" "}
+            </Dropdown.Toggle>{" "}
             <Dropdown.Menu style={{ minWidth: 370 }}>
-              <span style={{ padding: 10 }}>Cart is Empty!</span>
-            </Dropdown.Menu>
-          </Dropdown>
-        </Nav>
-      </Container>
+              {" "}
+              {cart.length > 0 ? (
+                <>
+                  {" "}
+                  {cart.map((prod) => (
+                    <span className="cartitem" key={prod.id}>
+                      <img
+                        src={prod.image}
+                        alt={prod.name}
+                        className="cartItemImg"
+                      />
+                      <div className="cartItemDetail">
+                        <span> {prod.name} </span>{" "}
+                        <span> $ {prod.price.split(".")[0]} </span>{" "}
+                      </div>{" "}
+                      <AiFillDelete
+                        fontSize="20px"
+                        style={{ cursor: "pointer" }}
+                        onClick={() => {
+                          dispatch({
+                            type: "REMOVE_FROM_CART",
+                            payload: prod,
+                          });
+                        }}
+                      />
+                    </span>
+                  ))}
+                  <Link to="/cart">
+                    <Button style={{ width: "95%", margin: "0 10px" }}>
+                      Go to Cart
+                    </Button>
+                  </Link>
+                </>
+              ) : (
+                <span style={{ padding: 10 }}> Cart is Empty! </span>
+              )}{" "}
+            </Dropdown.Menu>{" "}
+          </Dropdown>{" "}
+        </Nav>{" "}
+      </Container>{" "}
     </Navbar>
   );
 };
